cd78090cca07de0b07278babb452cb2b66bedb1d
Jakub Rychta <s27502@pjwstk.edu.pl>
2024-03-22 09:16:09 +0100

    Put age checking into AgeCheck method

--- zadanie_with_initial_tests/zadanie_solution/LegacyApp/UserService.cs
+++ zadanie_with_initial_tests/zadanie_solution/LegacyApp/UserService.cs
@@ -16,15 +16,11 @@ namespace LegacyApp
                 return false;
             }
 
-            var now = DateTime.Now;
-            int age = now.Year - dateOfBirth.Year;
-            if (now.Month < dateOfBirth.Month || (now.Month == dateOfBirth.Month && now.Day < dateOfBirth.Day)) age--;
-
-            if (age < 21)
+            if (!AgeCheck(dateOfBirth))
             {
                 return false;
             }
-
+            
             var clientRepository = new ClientRepository();
             var client = clientRepository.GetById(clientId);
 
@@ -59,13 +55,33 @@ namespace LegacyApp
                     user.CreditLimit = creditLimit;
                 }
             }
-            
+
             if (user.HasCreditLimit && user.CreditLimit < 500)
             {
                 return false;
             }
 
             UserDataAccess.AddUser(user);
+            return true;
+        }
+
+        public void CreditLimitCalculator(User user)
+        {
+            
+        }
+        public bool AgeCheck(DateTime dateOfBirth)
+        {
+            var now = DateTime.Now;
+            int age = now.Year - dateOfBirth.Year;
+            if (now.Month < dateOfBirth.Month || (now.Month == dateOfBirth.Month && now.Day < dateOfBirth.Day))
+            {
+                age--;
+            }
+            if (age < 21)
+            {
+                return false;
+            }
+
             return true;
         }
     }
